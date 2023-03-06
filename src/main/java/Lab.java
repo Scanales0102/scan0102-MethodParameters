ad806c0ecd5ea952e657b0c92cfdf095152f62e9
Sebastian Canales <spideronyou100@gmail.com>
2023-03-06 11:40:42 +0000

    completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,3 @@
-
 public class Lab {
     /**
      * This method should return whatever String was passed into it by whatever invoked it (in this case, the test),
@@ -25,7 +24,7 @@ public class Lab {
      * @param text is a parameter passed into this method by whatever called it.
      * @return text.
      */
-    public String copy(String text){
-        return "return text please!";
+    public String copy(String text) {
+        return text;
     }
 }
